eb407ef786b6ab5df1b958b2bf4ca4db43430a8b
dmarie777 <deimarie777@gmail.com>
2024-03-12 20:12:10 -0500

    Added functionality to add images

--- src/index.js
+++ src/index.js
@@ -4,3 +4,27 @@
  **/
 
 console.log('Happy hacking :)')
+const images = document.querySelector('.images');
+const button = document.querySelector('button')
+
+button.addEventListener('click', addFox)
+
+//Create a function to add random Foxes
+function addFox() {
+    const newImg = createImg()
+    images.appendChild(newImg);
+}
+
+//create an img node 
+function createImg() {
+    const maximum = 125;
+    const minimum = 1;
+    const random = Math.floor(Math.random()*(maximum-minimum));
+    const container = document.createElement('div')
+    container.className = 'container'
+    const img = document.createElement('img');
+    img.src = `https://randomfox.ca/images/${random}.jpg`;
+    container.appendChild(img)
+    return container
+}
+
